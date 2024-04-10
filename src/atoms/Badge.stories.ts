d2d72921bd0f2a13f9a8e899e93de038c610cb53
tct-willr <william.robertson@trout.com.au>
2024-04-10 16:31:49 +1000

    feat: badge styles

--- src/atoms/Badge.stories.ts
+++ src/atoms/Badge.stories.ts
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
 import { Badge } from "./Badge";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
@@ -7,11 +8,13 @@ const meta = {
   component: Badge,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    controls: { sort: "requiredFirst" },
     layout: "centered",
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ["autodocs"],
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+  args: { onClick: fn() },
 } satisfies Meta<typeof Badge>;
 
 export default meta;
@@ -21,15 +24,18 @@ type Story = StoryObj<typeof meta>;
 export const WithBorder: Story = {
   args: {
     label: "Badge",
-    color: "red",
+    color: "gray",
+    badgeStyle: "square",
     withBorder: true,
+    withDot: false,
   },
 };
 
 export const Flat: Story = {
   args: {
     label: "Badge",
-    color: "red",
+    badgeStyle: "square",
+    color: "gray",
     withBorder: false,
   },
 };
--- src/atoms/Badge.tsx
+++ src/atoms/Badge.tsx
@@ -1,28 +1,83 @@
 import { cn } from "@/utils";
 
-type SUPPORTED_COLORS = "red" | "blue";
+type SUPPORTED_COLORS = "gray" | "red" | "blue";
+
+type SUPPORTED_STYLES = "square" | "pill";
 
 interface BadgeProps {
-  // Badge contents
+  /**
+   * Badge contents
+   */
   label: string;
-  // What color should it be
+  /**
+   * What style is it?
+   */
+  badgeStyle: SUPPORTED_STYLES;
+  /**
+   * What color should it be?
+   */
   color: SUPPORTED_COLORS;
-  // Should it have a border?
-  withBorder: boolean;
+  /**
+   * Should it have a border?
+   */
+  withBorder?: boolean;
+  /**
+   * Should it have a dot?
+   */
+  withDot?: boolean;
+  /**
+   * Optional click handler
+   */
+  onClick?: () => void;
 }
 
-export function Badge({ label, color = "red", withBorder, ...props }: BadgeProps) {
+/**
+ * Small interactive or non-interactive displays
+ */
+export function Badge({
+  label,
+  color,
+  badgeStyle,
+  withBorder = false,
+  withDot = false,
+  onClick,
+  ...props
+}: BadgeProps) {
+  // with either dot or border or neither but not both
+  const shouldBeColored =
+    (withDot && !withBorder) || (withBorder && !withDot) || (!withBorder && !withDot);
+
   return (
     <span
       className={cn(
-        "inline-flex items-center rounded-md px-2 py-1",
-        withBorder && "ring-1 ring-inset",
-        {
-          "bg-red-50 text-red-700 ring-red-600/10": color === "red",
-          "bg-blue-50 text-blue-700 ring-blue-700/10": color === "blue",
-        }
+        "inline-flex cursor-pointer select-none items-center gap-x-1.5 rounded-md px-2 py-1",
+        { "rounded-md": badgeStyle === "square", "rounded-full": badgeStyle === "pill" },
+        // Add border to flat style if there is a dot
+        { "ring-1 ring-inset": withBorder || (!withBorder && withDot) },
+
+        shouldBeColored
+          ? {
+              "bg-gray-50 text-gray-600 ring-gray-500/10": color === "gray",
+              "bg-red-50 text-red-700 ring-red-600/10": color === "red",
+              "bg-blue-50 text-blue-700 ring-blue-700/10": color === "blue",
+            }
+          : ""
       )}
+      onClick={onClick}
     >
+      {withDot && (
+        <svg
+          className={cn("h-1.5 w-1.5", {
+            "fill-red-500": color === "red",
+            "fill-blue-500": color === "blue",
+          })}
+          viewBox="0 0 6 6"
+          aria-hidden="true"
+        >
+          <circle cx={3} cy={3} r={3} />
+        </svg>
+      )}
+
       {label}
     </span>
   );
