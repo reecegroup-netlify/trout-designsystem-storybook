0ba6e56039dca0c7c115ff9d7129c04f25c7396a
tct-willr <william.robertson@trout.com.au>
2024-04-16 19:41:38 +1000

    refactor: remove default tw colors
    We will use design tokens for this which will wholy override tw's palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,8 +22,8 @@ const config: Config = {
     "./src/modules/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: customColors,
     extend: {
-      colors: customColors,
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
