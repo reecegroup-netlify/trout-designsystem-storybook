ef9fb187907f89388de8400a6504823fec4f8ecf
tct-willr <william.robertson@trout.com.au>
2024-04-26 10:20:11 +1000

    feat: alphabetise story sorting

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,6 +3,11 @@ import "../src/styles/globals.css";
 
 const preview: Preview = {
   parameters: {
+    options: {
+      storySort: {
+        method: "alphabetical",
+      },
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
