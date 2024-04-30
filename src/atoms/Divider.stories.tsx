c58e6df4e15d298696fb19c47c6028eba6f85775
tct-willr <william.robertson@trout.com.au>
2024-04-30 13:45:19 +1000

    feat: refactor divider, add icon variant

--- src/atoms/Divider.stories.tsx
+++ src/atoms/Divider.stories.tsx
@@ -1,20 +1,22 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import Divider, { LabelDivider } from "./Divider";
+import { Divider } from "./Divider";
+import { PlusCircleIcon } from "@heroicons/react/24/solid";
 
 const meta = {
   title: "Atoms/Divider",
-  component: LabelDivider,
+  component: Divider,
   parameters: {
-    // Order the controls
     controls: { sort: "requiredFirst" },
-    // Center component
   },
-  // tags: ["autodocs"],
-} satisfies Meta<typeof LabelDivider>;
+} satisfies Meta<typeof Divider>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const LabelledDivider: Story = {
-  args: { labelText: "Continue", labelLocation: "center", labelType: "label" },
+  args: { variant: "text", labelText: "Continue", labelLocation: "center", labelType: "label" },
+};
+
+export const IconDivider: Story = {
+  args: { variant: "icon", labelLocation: "center", icon: <PlusCircleIcon className="h-5" /> },
 };
--- src/atoms/Divider.tsx
+++ src/atoms/Divider.tsx
@@ -1,49 +1,53 @@
 import { cn } from "@/utils";
 
-type DividerProps = {
-  /**
-   * Divider text
-   */
-  labelText?: string;
-  /**
-   * Large or small text
-   */
+type TextDividerProps = {
+  variant: "text";
+  labelText: string;
   labelType: "label" | "title";
   labelLocation: "left" | "center" | "right";
 };
 
-export default function Divider() {
-  return (
-    <div className="relative">
-      <div className="absolute inset-0 flex items-center" aria-hidden="true">
-        <div className="w-full border-t bg-black" />
-      </div>
-    </div>
-  );
-}
+type IconDividerProps = {
+  variant: "icon";
+  icon: JSX.Element;
+  labelLocation: "left" | "center" | "right";
+};
 
-export function LabelDivider({ labelText, labelType, labelLocation }: DividerProps) {
-  const labelLocationStyle = {
-    "justify-start": labelLocation === "left",
-    "justify-center": labelLocation === "center",
-    "justify-end": labelLocation === "right",
-  };
+type NoneDividerProps = {
+  variant: "none";
+};
 
-  const textWeightStyle = {
-    "text-sm text-gray-500": labelType === "label",
-    "text-base font-semibold text-gray-900": labelType === "label",
-  };
+type DividerProps = TextDividerProps | IconDividerProps | NoneDividerProps;
+
+export function Divider(props: DividerProps) {
+  const { variant } = props;
 
   return (
     <div className="relative">
       <div className="absolute inset-0 flex items-center" aria-hidden="true">
         <div className="w-full border-t bg-black" />
       </div>
-      <div className={cn("relative flex", labelLocationStyle)}>
-        <span className={cn("bg-white px-2", textWeightStyle)}>{labelText}</span>
-      </div>
+      {variant !== "none" && (
+        <div
+          className={cn("relative flex", {
+            "justify-start": props.labelLocation === "left",
+            "justify-center": props.labelLocation === "center",
+            "justify-end": props.labelLocation === "right",
+          })}
+        >
+          {variant === "text" && (
+            <span
+              className={cn("bg-white px-2", {
+                "text-sm text-gray-300": props.labelType === "label",
+                "text-base font-semibold text-gray-900": props.labelType === "label",
+              })}
+            >
+              {props.labelText}
+            </span>
+          )}
+          {variant === "icon" && <span className="bg-white px-2">{props.icon}</span>}
+        </div>
+      )}
     </div>
   );
 }
-
-<LabelDivider labelText="label" labelType="label" labelLocation="left" />;
