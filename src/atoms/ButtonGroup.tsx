c94d01a4d073f075a5a6c755b30534e013c99e2d
tct-willr <william.robertson@trout.com.au>
2024-04-30 16:24:50 +1000

    fix: lint styles

--- src/atoms/ButtonGroup.tsx
+++ src/atoms/ButtonGroup.tsx
@@ -19,7 +19,7 @@ export function ButtonGroup({ groupItems }: ButtonGroupProps) {
           <button
             type="button"
             className={cn(
-              "bg-white text-secondary-900 ring-secondary-300 hover:bg-secondary-100 relative inline-flex items-center px-3 py-2 text-sm font-semibold ring-1 ring-inset focus:z-10",
+              "text-secondary-900 ring-secondary-300 hover:bg-secondary-100 relative inline-flex items-center bg-white px-3 py-2 text-sm font-semibold ring-1 ring-inset focus:z-10",
               isFirst ? "rounded-l-md" : "-ml-px",
               isLast ? "rounded-r-md" : ""
             )}
